6b1b660eb680c1a35db18d60885104b680b8dff6
王平 <1540703192@qq.com>
2021-03-23 12:11:28 +0800

    init a plugin

---
+++ main.js
@@ -0,0 +1,9 @@
+utools.onPluginReady(() => {
+    utools.showMessageBox({
+        type: 'info',
+        buttons: ['nice'],
+        title: 'hello',
+        message: 'awesome',
+        defaultId: 1,
+    });
+});
